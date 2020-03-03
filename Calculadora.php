58806b13869ebfb15d2855761d83832051c8a8ea
Paulo Roberto Torres <nepanuceno@gmail.com>
2020-03-03 10:51:49 -0300

    Create Calculadora.php

---
+++ Calculadora.php
@@ -0,0 +1,52 @@
+<?php
+
+class Calculadora {
+    private function requisicao($params, $sUrl){
+
+        try{
+            $ctx = stream_context_create($params);
+            $fp = @fopen($sUrl, 'rb', false, $ctx);
+            $response = @stream_get_contents($fp);
+        }
+        catch(Exception $e) {
+            echo 'Message: ' .$e->getMessage();
+        }
+
+        return $response;
+    }
+
+    public function calcular($url, $method, $content){
+
+        $params = array('http' => array(
+            'method' => 'POST',
+            'content' => 'grau=1&tipo=1&precatorio=0&idNatureza=82&tipoPreparo=1&valorCausa=1100&validadoEscrivania=true&validadoSecretaria=true&quantidadeDistribuicao=2&quantidadeDeRegistros=1&validadoDistribuicao=true&urbana=0&suburbana=0&rural=0&urbanaHC=0&suburbanaHC=0&ruralHC=0&mesmoMandado=0&mandadoDiferente=0&quantidadePostais=0&validadoOficialJustica=true&validado=true'
+        ));
+
+        $sUrl = "http://20.14.3.152/calculadora/Home/PreCalculo";
+        return $this->requisicao($params, $sUrl);
+    }
+
+    public function gerarHash(){
+
+
+        $params = array('http' => array(
+            'method' => 'POST',
+            'content' => 'objCalculo[grau]=1&objCalculo[tipo]=1&objCalculo[precatorio]=0&objCalculo[idNatureza]=82&objCalculo[tipoPreparo]=1&objCalculo[valorCausa]=1100&objCalculo[observacao]=false&objCalculo[validadoEscrivania]=true&objCalculo[validadoSecretaria]=true&objCalculo[quantidadeDistribuicao]=2&objCalculo[quantidadeDeRegistros]=1&objCalculo[validadoDistribuicao]=true&objCalculo[urbana]=0&objCalculo[suburbana]=0&objCalculo[rural]=0&objCalculo[urbanaHC]=0&objCalculo[suburbanaHC]=0&objCalculo[ruralHC]=0&objCalculo[mesmoMandado]=0&objCalculo[mandadoDiferente]=0&objCalculo[quantidadePostais]=0&objCalculo[validadoOficialJustica]=true'
+        ));
+
+        $sUrl = "http://20.14.3.152/calculadora/Home/Salvar";
+        return $this->requisicao($params, $sUrl);
+    }
+
+
+    public function gerarCodigo($hash) {
+
+        $params = array('http' => array(
+            'method' => 'GET',
+            'content' => '',
+        ));
+        $sUrl = "http://20.14.3.152/calculadora/Home/Visualizar/$hash?v=1";
+
+        return $this->requisicao($params, $sUrl);
+    }
+}
